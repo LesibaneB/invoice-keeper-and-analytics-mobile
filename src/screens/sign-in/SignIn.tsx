5ce87cc27f1a5f5d8015cc3c359c35b76710aaa8
bonakelelesibane <bonakele.lesibane@gmail.com>
2021-03-31 01:18:42 +0200

    Added validation and logo.

--- src/screens/sign-in/SignIn.tsx
+++ src/screens/sign-in/SignIn.tsx
@@ -1,4 +1,4 @@
-import { StackNavigationProp } from '@react-navigation/stack';
+import {StackNavigationProp} from '@react-navigation/stack';
 import {
   Button,
   Container,
@@ -6,51 +6,125 @@ import {
   Form,
   Input,
   Item,
-  Label,
   Text,
+  View,
 } from 'native-base';
 import React from 'react';
+import {Controller, useForm} from 'react-hook-form';
 import {StyleSheet} from 'react-native';
 import {
   responsiveFontSize,
   responsiveHeight,
   responsiveWidth,
 } from 'react-native-responsive-dimensions';
-import { RootStackParamList } from '../../../App';
+import {RootStackParamList} from '../../../App';
+import * as yup from 'yup';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {InputError} from '../../components/InputError';
+import {
+  EMAIL_FORMAT_NOT_VALID,
+  PASSWORD_LENGTH_NOT_VALID,
+} from '../../utils/messages';
+import Logo from '../../images/Invoice Scanner Logo.svg';
 
-type SignInNavigationProp = StackNavigationProp<
-  RootStackParamList,
-  'SignIn'
->;
+type SignInNavigationProp = StackNavigationProp<RootStackParamList, 'SignIn'>;
 
 interface Props {
-    navigation: SignInNavigationProp;
+  navigation: SignInNavigationProp;
 }
 
+interface FormData {
+  email: string;
+  password: string;
+}
+
+const schema = yup.object().shape({
+  email: yup.string().email(EMAIL_FORMAT_NOT_VALID),
+  password: yup.string().min(6, PASSWORD_LENGTH_NOT_VALID),
+});
+
 export function SignIn({}: Props): JSX.Element {
+  const {control, handleSubmit, errors} = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  function submit(data: FormData) {
+    console.log(data);
+  }
+
+  function navigateToForgotPassword() {
+    console.log('Push to Forgot Password.');
+  }
+
+  function navigateToSignUp() {
+    console.log('Push to Sign Up.');
+  }
+
   return (
-    <Container>
+    <Container style={{flex: 1}}>
       <Content style={styles.contentContainer}>
-        <Text style={styles.logo}> Logo Here</Text>
-        <Text style={styles.instruction}>Login to your account</Text>
+        <View style={styles.logo}>
+          <Logo width={100} height={100} />
+        </View>
+        <Text style={styles.instruction}>Sign in to your account.</Text>
         <Form>
-          <Item regular last style={styles.input}>
-            <Input placeholder="Email" />
-          </Item>
-          <Item regular last style={styles.input}>
-            <Input placeholder="Password" />
-          </Item>
+          <Controller
+            control={control}
+            render={({onBlur, value, onChange}) => (
+              <>
+                <Item regular last style={styles.input}>
+                  <Input
+                    placeholder="Email"
+                    value={value}
+                    onBlur={onBlur}
+                    onChangeText={(value) => onChange(value)}
+                  />
+                </Item>
+                {errors.email && <InputError message={errors.email?.message} />}
+              </>
+            )}
+            name="email"
+            defaultValue=""
+          />
+          <Controller
+            control={control}
+            render={({onBlur, onChange, value}) => (
+              <>
+                <Item regular last style={styles.input}>
+                  <Input
+                    placeholder="Password"
+                    value={value}
+                    onBlur={onBlur}
+                    onChangeText={(value) => onChange(value)}
+                    secureTextEntry={true}
+                  />
+                </Item>
+                {errors.password && (
+                  <InputError message={errors.password?.message} />
+                )}
+              </>
+            )}
+            name="password"
+            defaultValue=""
+          />
           <Text
             style={styles.forgotPasswordText}
-            onPress={() => console.log('hello there')}>
+            onPress={navigateToForgotPassword}>
             Forgot password?
           </Text>
-          <Button block style={styles.signInButton}>
+          <Button
+            block
+            style={styles.signInButton}
+            onPress={handleSubmit(submit)}>
             <Text uppercase={false}>Sign In</Text>
           </Button>
           <Text style={styles.noAccountTextFirstPart}>
             Don't have an account?{' '}
-            <Text style={styles.noAccountTextSecondPart}>Sign up.</Text>
+            <Text
+              onPress={navigateToSignUp}
+              style={styles.noAccountTextSecondPart}>
+              Sign Up.
+            </Text>
           </Text>
         </Form>
       </Content>
@@ -62,9 +136,10 @@ const styles = StyleSheet.create({
   contentContainer: {
     marginLeft: responsiveWidth(8),
     marginRight: responsiveWidth(8),
+    flex: 1,
   },
   logo: {
-    marginTop: responsiveHeight(15),
+    marginTop: responsiveHeight(10),
     flex: 1,
     alignSelf: 'center',
   },
@@ -83,12 +158,13 @@ const styles = StyleSheet.create({
     color: '#0000EE',
   },
   signInButton: {
-    backgroundColor: 'blue',
-    marginTop: responsiveHeight(3),
+    backgroundColor: '#321AC6',
+    marginTop: responsiveHeight(2),
     borderRadius: 5,
   },
   noAccountTextFirstPart: {
     marginTop: responsiveHeight(2),
+    color: '#000000',
   },
   noAccountTextSecondPart: {
     color: '#0000EE',
---
+++ src/utils/messages.ts
@@ -0,0 +1,2 @@
+export const EMAIL_FORMAT_NOT_VALID = 'Please enter email address in the format: yourname@example.com.';
+export const PASSWORD_LENGTH_NOT_VALID =  'The password must be at least 6 characters.';
